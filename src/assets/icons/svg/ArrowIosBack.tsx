ae5a005f3b21270bb7e4eb721802549e760cd50f
Daniil <daniilminin1990@yandex.ru>
2024-05-02 20:43:51 +0500

    add ArrowIosBack

--- src/assets/icons/svg/ArrowIosBack.tsx
+++ src/assets/icons/svg/ArrowIosBack.tsx
@@ -1,27 +1,16 @@
 import type { SVGProps } from 'react'
+// eslint-disable-next-line no-duplicate-imports
 import { Ref, forwardRef, memo } from 'react'
 const SvgArrowIosBack = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
-  <svg
-    fill={'none'}
-    height={24}
-    ref={ref}
-    width={24}
-    xmlns={'http://www.w3.org/2000/svg'}
-    {...props}
-  >
-    <g clipPath={'url(#arrow-ios-back_svg__a)'}>
+  <svg ref={ref} viewBox={'0 0 24 24'} xmlns={'http://www.w3.org/2000/svg'} {...props}>
+    <g data-name={'Layer 2'}>
       <path
         d={
-          'M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1.001 1.001 0 1 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64'
+          'M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64'
         }
-        fill={'currentcolor'}
+        data-name={'arrow-ios-back'}
       />
     </g>
-    <defs>
-      <clipPath id={'arrow-ios-back_svg__a'}>
-        <path d={'M0 0h24v24H0z'} fill={'currentcolor'} />
-      </clipPath>
-    </defs>
   </svg>
 )
 const ForwardRef = forwardRef(SvgArrowIosBack)
