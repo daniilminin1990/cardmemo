eb51094a0c585716a0ddc5141b4e2ed9e8556e9e
andreyshalomitsky <andreyshalomitsky@gmail.com>
2024-05-04 14:53:36 +0500

    changed App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { Button } from '@/components/ui/button'
-import { Card } from '@/components/ui/card'
 
 export function App() {
   return (
     <div>
       <Button>123 </Button>
-      <Card>
-        <div>Hi</div>
-      </Card>
     </div>
   )
 }
