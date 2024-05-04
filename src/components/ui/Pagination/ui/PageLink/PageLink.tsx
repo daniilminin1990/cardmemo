ffa171b98115dccb0890b0dbb77cba4741c658f0
Daniil <129886977+daniilminin1990@users.noreply.github.com>
2024-05-05 03:26:44 +0500

    add PaginationWithSelect, demo, Pagination, PageLink, styles, stories, and refactor textSize for Select
    * refactor styles for pagination
    
    * add icons
    refactor pagination with clsx,
    moved state inside Pagination
    
    * refactor folder and components title Uppercase
    
    * add story for Pagination
    
    * refactor title pagination.module.scss
    
    * add PaginationWithSelect, demo, Pagination, PageLink, styles, stories, and refactor textSize for Select

---
+++ src/components/ui/Pagination/ui/PageLink/PageLink.tsx
@@ -0,0 +1,28 @@
+import { HTMLProps } from 'react'
+
+import clsx from 'clsx'
+
+import s from '../pagination.module.scss'
+
+type Props = { active?: boolean } & HTMLProps<HTMLAnchorElement>
+
+export const PageLink = ({ active, children, className, disabled, ...props }: Props) => {
+  // const customClassName = `${s.PageLink} ${className} ${active ? s.active : ''} ${
+  const customClassName = clsx(s.pageLink, className, {
+    [s.active]: active,
+    [s.disabled]: disabled,
+  })
+
+  return (
+    <div className={s.pageLinkWrapper}>
+      {disabled ? (
+        // <span className={`${customClassName} ${s.PageLink}`}>{children}</span>
+        <span className={clsx(customClassName, s.pageLink)}>{children}</span>
+      ) : (
+        <a {...props} aria-current={active ? 'page' : undefined} className={customClassName}>
+          {children}
+        </a>
+      )}
+    </div>
+  )
+}
