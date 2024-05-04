37196a8397ba35c8c197c8fa4912567bddd37fa2
Daniil <daniilminin1990@yandex.ru>
2024-05-04 19:23:31 +0500

    add TabSwitcher, styles, storybook and demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { TabSwitcherDemo } from '@/components/ui/TabSwitcherDemo'
 import { Button } from '@/components/ui/button'
 
 export function App() {
   return (
     <div>
       <Button>123 </Button>
+      <TabSwitcherDemo />
     </div>
   )
 }
