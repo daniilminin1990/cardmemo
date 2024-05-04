19eab08a65e66453f67798d52eb721e8a7af314c
Daniil <daniilminin1990@yandex.ru>
2024-05-05 02:43:32 +0500

    remove SelectDemo from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { SelectDemo } from '@/components/ui/SelectDemo'
 import { Button } from '@/components/ui/button'
 
 export function App() {
   return (
     <div>
       <Button>123 </Button>
-      <div>
-        <SelectDemo />
-      </div>
     </div>
   )
 }
