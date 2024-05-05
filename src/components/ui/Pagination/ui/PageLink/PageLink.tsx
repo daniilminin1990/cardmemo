186d5ac094227caad958b2d029d758c0c2b58e25
Daniil <daniilminin1990@yandex.ru>
2024-05-05 21:32:09 +0500

    refactor Pagination PageLink and styles, replace elements with Typography, so refactored Typography

--- src/components/ui/Pagination/ui/PageLink/PageLink.tsx
+++ src/components/ui/Pagination/ui/PageLink/PageLink.tsx
@@ -1,13 +1,18 @@
-import { HTMLProps } from 'react'
+import { HTMLProps, ReactNode } from 'react'
 
+import Typography from '@/components/ui/Typography/Typography'
 import clsx from 'clsx'
 
 import s from '../pagination.module.scss'
 
-type Props = { active?: boolean } & HTMLProps<HTMLAnchorElement>
+type Props = {
+  active?: boolean
+  children?: ReactNode
+  className?: string
+  disabled?: boolean
+} & HTMLProps<HTMLAnchorElement>
 
 export const PageLink = ({ active, children, className, disabled, ...props }: Props) => {
-  // const customClassName = `${s.PageLink} ${className} ${active ? s.active : ''} ${
   const customClassName = clsx(s.pageLink, className, {
     [s.active]: active,
     [s.disabled]: disabled,
@@ -16,12 +21,20 @@ export const PageLink = ({ active, children, className, disabled, ...props }: Pr
   return (
     <div className={s.pageLinkWrapper}>
       {disabled ? (
-        // <span className={`${customClassName} ${s.PageLink}`}>{children}</span>
-        <span className={clsx(customClassName, s.pageLink)}>{children}</span>
-      ) : (
-        <a {...props} aria-current={active ? 'page' : undefined} className={customClassName}>
+        <Typography as={'p'} className={clsx(customClassName, s.pageLink)} variant={'body1'}>
           {children}
-        </a>
+        </Typography>
+      ) : (
+        <Typography
+          {...props}
+          aria-current={active ? 'page' : undefined}
+          as={'a'}
+          className={customClassName}
+          target={'_self'}
+          variant={'link1'}
+        >
+          {children}
+        </Typography>
       )}
     </div>
   )
