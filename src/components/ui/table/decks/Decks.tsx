b062e22f8b4fd2d687c60fe1485b6dd89b2b483f
BladeDancer <69kir969@mail.ru>
2024-05-05 19:35:28 +0300

    add sort and sorticons , changed the items array structure

--- src/components/ui/table/decks/Decks.tsx
+++ src/components/ui/table/decks/Decks.tsx
@@ -1,3 +1,8 @@
+import { useMemo } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+import ArrowIosDownOutline from '@/assets/icons/svg/ArrowIosDownOutline'
+import ArrowIosUp from '@/assets/icons/svg/ArrowIosUp'
 import Edit2Outline from '@/assets/icons/svg/Edit2Outline'
 import PlayCircleOutline from '@/assets/icons/svg/PlayCircleOutline'
 import TrashOutline from '@/assets/icons/svg/TrashOutline'
@@ -7,73 +12,144 @@ import s from './decks.module.scss'
 import defaltDeckImg from '../../../../assets/img/defaultDeckImg.png'
 import { Table } from '../table'
 
-type DecksProps = {
+export type DecksProps = {
+  author: {
+    id: string
+    name: string
+  }
   cardsCount: number
+  cover: string
   created: string
   id: string
+  isPrivate: boolean
   name: string
   updated: string
+  userId: string
 }
 
 export const Decks = () => {
+  const headersName = ['Name', 'Cards', 'Last Updated', 'Created By']
   const items: DecksProps[] = [
     {
-      cardsCount: 1,
-      created: 'BD',
-      id: '1',
-      name: 'Hello World',
+      author: {
+        id: '1',
+        name: 'BD',
+      },
+      cardsCount: 0,
+      cover: '…',
+      created: '02.05.2024',
+      id: 'c1',
+      isPrivate: true,
+      name: 'My Deck',
       updated: '02.05.2024',
+      userId: '1',
     },
     {
-      cardsCount: 2,
-      created: 'BD',
-      id: '2',
-      name: 'Hello World 1',
-      updated: '02.05.2024',
+      author: {
+        id: '2',
+        name: 'Author name',
+      },
+      cardsCount: 8,
+      cover: '…',
+      created: '02.05.2024',
+      id: 'c2',
+      isPrivate: false,
+      name: 'Other Deck',
+      updated: '12.02.2022',
+      userId: '1',
     },
     {
-      cardsCount: 3,
-      created: 'BD',
-      id: '3',
-      name: 'Hello World 2',
-      updated: '02.05.2024',
+      author: {
+        id: '3',
+        name: 'Other7',
+      },
+      cardsCount: 5,
+      cover: '…',
+      created: '17.02.2024',
+      id: 'c3',
+      isPrivate: false,
+      name: 'Z',
+      updated: '17.02.2023',
+      userId: '1',
+    },
+    {
+      author: {
+        id: '4',
+        name: 'Other1',
+      },
+      cardsCount: 0,
+      cover: '…',
+      created: '17.02.2023',
+      id: 'c4',
+      isPrivate: false,
+      name: 'C',
+      updated: '22.02.2023',
+      userId: '1',
     },
   ]
+  const [searchParams, setSearchParams] = useSearchParams()
+  const activeColumn = searchParams.get('sort') || 'Last Updated'
+  const sortOrder = searchParams.get('order') || 'asc'
+
+  const handleSort = (columnName: any) => {
+    const order = activeColumn === columnName && sortOrder === 'asc' ? 'desc' : 'asc'
+
+    setSearchParams({ order, sort: columnName })
+  }
+
+  const columnKeys = {
+    Cards: 'cardsCount',
+    'Created By': 'author.name',
+    'Last Updated': 'updated',
+    Name: 'name',
+  }
+
+  const sortedItems = useMemo(() => {
+    //@ts-ignore
+    if (!activeColumn || !columnKeys[activeColumn]) {
+      return items
+    }
+    //@ts-ignore
+    const key = columnKeys[activeColumn]
+
+    return [...items].sort((a, b) => {
+      const aValue = key.split('.').reduce((o: string, i: number) => o[i], a)
+      const bValue = key.split('.').reduce((o: string, i: number) => o[i], b)
+
+      if (aValue < bValue) {
+        return sortOrder === 'asc' ? -1 : 1
+      }
+      if (aValue > bValue) {
+        return sortOrder === 'asc' ? 1 : -1
+      }
+
+      return 0
+    })
+  }, [items, activeColumn, sortOrder, columnKeys])
 
   return (
     <Table.Root>
       <Table.Head>
         <Table.Row>
-          <Table.HeadCell>
-            <div>
-              <span>Name {/*add sort icon*/}</span>
-            </div>
-          </Table.HeadCell>
-          <Table.HeadCell>
-            <div>
-              <span>Cards{/*add sort icon*/}</span>
-            </div>
-          </Table.HeadCell>
-          <Table.HeadCell>
-            <div>
-              <span>Last Updated {/*add sort icon*/}</span>
-            </div>
-          </Table.HeadCell>
-          <Table.HeadCell>
-            <div>
-              <span>Created By {/*add sort icon*/}</span>
-            </div>
-          </Table.HeadCell>
-          <Table.HeadCell>
-            <div>
-              <span> {/*add sort icon*/}</span>
-            </div>
-          </Table.HeadCell>
+          {headersName.map(name => (
+            <Table.HeadCell className={s.visible} key={name} onClick={() => handleSort(name)}>
+              <span>
+                {name}
+                {activeColumn === name && sortOrder === 'desc' && (
+                  <ArrowIosUp className={s.arrow} />
+                )}
+                {activeColumn === name && sortOrder === 'asc' && (
+                  <ArrowIosDownOutline className={s.arrow} />
+                )}
+              </span>
+            </Table.HeadCell>
+          ))}
+          <Table.HeadCell></Table.HeadCell>
         </Table.Row>
       </Table.Head>
       <Table.Body>
-        {items.length !== 0 ? (
-          items.map(item => (
+        {sortedItems.length !== 0 ? (
+          sortedItems.map(item => (
             <Table.Row key={item.id}>
               <Table.Cell>
                 <a className={s.nameBlock} href={'/'}>
@@ -83,25 +159,39 @@ export const Decks = () => {
               </Table.Cell>
               <Table.Cell>{item.cardsCount}</Table.Cell>
               <Table.Cell>{item.updated}</Table.Cell>
-              <Table.Cell>{item.created}</Table.Cell>
+              <Table.Cell>{item.author.name}</Table.Cell>
               <Table.Cell>
-                <div className={s.iconBtns}>
-                  <button>
-                    <PlayCircleOutline className={s.playCircleOutline} viewBox={'0 0 24 24'} />
-                  </button>
-                  <button>
-                    <Edit2Outline className={s.Edit2Outline} viewBox={'0 0 24 24'} />
-                  </button>
-                  <button>
-                    <TrashOutline className={s.TrashOutline} viewBox={'0 0 24 24'} />
-                  </button>
-                </div>
+                {item.userId === item.author.id ? (
+                  <div className={s.iconBtns}>
+                    <button>
+                      <Edit2Outline className={s.Edit2Outline} />
+                    </button>
+                    <button disabled={item.cardsCount === 0}>
+                      <PlayCircleOutline
+                        className={`${s.playCircleOutline} ${item.cardsCount === 0 && s.disabled}`}
+                      />
+                    </button>
+                    <button>
+                      <TrashOutline className={s.TrashOutline} />
+                    </button>
+                  </div>
+                ) : (
+                  <div className={s.iconBtns}>
+                    <button disabled={item.cardsCount === 0}>
+                      <PlayCircleOutline
+                        className={`${s.playCircleOutline} ${item.cardsCount === 0 && s.disabled}`}
+                      />
+                    </button>
+                  </div>
+                )}
               </Table.Cell>
             </Table.Row>
           ))
         ) : (
           <Table.Row>
-            <Table.Cell colSpan={100}>Empty</Table.Cell>
+            <Table.Cell colSpan={100} style={{ textAlign: 'center' }}>
+              Empty
+            </Table.Cell>
           </Table.Row>
         )}
       </Table.Body>
--- src/components/ui/table/table.tsx
+++ src/components/ui/table/table.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps } from 'react'
+import { ComponentProps, forwardRef } from 'react'
 
 import { clsx } from 'clsx'
 
@@ -6,60 +6,64 @@ import s from './table.module.scss'
 
 export type RootProps = ComponentProps<'table'>
 
-export const Root = ({ className, ...rest }: RootProps) => {
+export const Root = forwardRef<HTMLTableElement, RootProps>(({ className, ...rest }, ref) => {
   const classNames = {
     table: clsx(className, s.table),
   }
 
-  return <table className={classNames.table} {...rest} />
-}
+  return <table className={classNames.table} ref={ref} {...rest} />
+})
 
 export type HeadProps = ComponentProps<'thead'>
 
-export const Head = ({ className, ...props }: HeadProps) => {
-  const classNames = {
-    thead: clsx(className, s.thead),
+export const Head = forwardRef<HTMLTableSectionElement, HeadProps>(
+  ({ className, ...props }: HeadProps, ref) => {
+    const classNames = {
+      thead: clsx(className, s.thead),
+    }
+
+    return <thead className={classNames.thead} {...props} ref={ref} />
   }
-
-  return <thead className={classNames.thead} {...props} />
-}
-
+)
 export type BodyProps = ComponentProps<'tbody'>
 
-export const Body = ({ className, ...props }: BodyProps) => {
-  const classNames = {
-    tbody: clsx(className, s.tbody),
+export const Body = forwardRef<HTMLTableSectionElement, BodyProps>(
+  ({ className, ...props }: BodyProps, ref) => {
+    const classNames = {
+      tbody: clsx(className, s.tbody),
+    }
+
+    return <tbody className={classNames.tbody} {...props} ref={ref} />
   }
-
-  return <tbody className={classNames.tbody} {...props} />
-}
-
+)
 export type RowProps = ComponentProps<'tr'>
 
-export const Row = ({ ...props }: RowProps) => {
-  return <tr {...props} />
-}
-
+export const Row = forwardRef<HTMLTableRowElement, RowProps>(({ ...props }: RowProps, ref) => {
+  return <tr {...props} ref={ref} />
+})
 export type HeadCellProps = ComponentProps<'th'>
 
-export const HeadCell = ({ className, ...rest }: HeadCellProps) => {
-  const classNames = {
-    headCell: clsx(className, s.headCell),
-  }
+export const HeadCell = forwardRef<HTMLTableCellElement, HeadCellProps>(
+  ({ className, ...rest }: HeadCellProps, ref) => {
+    const classNames = {
+      headCell: clsx(className, s.headCell),
+    }
 
-  return <th className={classNames.headCell} {...rest} />
-}
+    return <th className={classNames.headCell} {...rest} ref={ref} />
+  }
+)
 
 export type CellProps = ComponentProps<'td'>
 
-export const Cell = ({ className, ...rest }: CellProps) => {
-  const classNames = {
-    cell: clsx(className, s.cell),
+export const Cell = forwardRef<HTMLTableCellElement, CellProps>(
+  ({ className, ...rest }: CellProps, ref) => {
+    const classNames = {
+      cell: clsx(className, s.cell),
+    }
+
+    return <td className={classNames.cell} {...rest} ref={ref} />
   }
-
-  return <td className={classNames.cell} {...rest} />
-}
-
+)
 export const Table = {
   Body,
   Cell,
