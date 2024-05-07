fac137c79a150b34d344a94db11ed28afefdbb5c
BladeDancer <69kir969@mail.ru>
2024-05-07 21:58:04 +0300

    create forms and storybook

---
+++ src/components/auth/forgot-pass/check-email/checkEmail.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { CheckEmail } from '@/components/auth/forgot-pass/check-email/checkEmail'
+
+const meta = {
+  component: CheckEmail,
+  title: 'Components/Auth/ForgotPassword/CheckEmail',
+} satisfies Meta<typeof CheckEmail>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
