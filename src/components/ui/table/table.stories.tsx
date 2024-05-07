1ae9633c92afe2f5d13d24cc4924f5e255708c92
Kirill <112247196+kirabortz@users.noreply.github.com>
2024-05-08 00:34:30 +0300

    Table v2 (#51)
    fix props classname, add "No content"

--- src/components/ui/table/table.stories.tsx
+++ src/components/ui/table/table.stories.tsx
@@ -1,6 +1,9 @@
 import { MemoryRouter } from 'react-router-dom'
 
+import Typography from '@/components/ui/Typography/Typography'
 import { Decks } from '@/components/ui/table/decks/Decks'
+
+import s from '@/components/ui/table/decks/decks.module.scss'
 
 import { Table } from './table'
 
@@ -13,30 +16,35 @@ const meta = {
 export default meta
 
 export const Default = () => {
-  const headersName = ['Name', 'Cards', 'Last Updated', 'Created By']
-
   return (
-    <MemoryRouter initialEntries={['/']}>
-      <Table.Root>
-        <Table.Head>
-          <Table.Row>
-            {headersName.map(name => (
-              <Table.HeadCell key={name}>
-                <span>{name}</span>
-              </Table.HeadCell>
-            ))}
-            <Table.HeadCell></Table.HeadCell>
-          </Table.Row>
-        </Table.Head>
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell colSpan={100} style={{ textAlign: 'center' }}>
-              Empty
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table.Root>
-    </MemoryRouter>
+    <Typography as={'div'} className={s.empty} variant={'body1'}>
+      No content with these terms...
+    </Typography>
+
+    /** displaying an empty table**/
+    // const headersName = ['Name', 'Cards', 'Last Updated', 'Created By']
+
+    // <MemoryRouter initialEntries={['/']}>
+    //   <Table.Root>
+    //     <Table.Head>
+    //       <Table.Row>
+    //         {headersName.map(name => (
+    //           <Table.HeadCell key={name}>
+    //             <span>{name}</span>
+    //           </Table.HeadCell>
+    //         ))}
+    //         <Table.HeadCell></Table.HeadCell>
+    //       </Table.Row>
+    //     </Table.Head>
+    //     <Table.Body>
+    //       <Table.Row>
+    //         <Table.Cell className={`empty`} colSpan={100}>
+    //           No content with these terms...
+    //         </Table.Cell>
+    //       </Table.Row>
+    //     </Table.Body>
+    //   </Table.Root>
+    // </MemoryRouter>
   )
 }
 export const WithContent = () => {
