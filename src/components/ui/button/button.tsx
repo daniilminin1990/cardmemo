647c97e9d088200e6dc13c11d35b79a4a1f7373e
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2024-05-10 01:47:40 +0300

    feature: adding forwardRef to button.tsx

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+import { ComponentPropsWithoutRef, ElementType, ReactNode, forwardRef } from 'react'
 
 import s from './button.module.scss'
 
@@ -10,13 +10,14 @@ export type ButtonProps<T extends ElementType = 'button'> = {
   variant?: 'primary' | 'secondary'
 } & ComponentPropsWithoutRef<T>
 
-export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
   const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
 
   return (
     <Component
       className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
+      ref={ref}
       {...rest}
     />
   )
-}
+})
