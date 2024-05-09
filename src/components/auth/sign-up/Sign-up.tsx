bfd3a0c11edd1161b928c46149c2ea48c9c1d63f
Siarhei Haurylchyk <sggavrilchik@gmail.com>
2024-05-09 21:05:47 +0300

    start task

--- src/components/auth/sign-up/Sign-up.tsx
+++ src/components/auth/sign-up/Sign-up.tsx
@@ -8,9 +8,9 @@ import { Card } from '@/components/ui/card'
 import style from '@/components/auth/sign-up/SignUp.module.scss'
 
 type FormValue = {
+  confirmPassword: string
   login: string
   password: string
-  confirmPassword: string
 }
 
 export default function SignUp() {
@@ -27,45 +27,45 @@ export default function SignUp() {
     <form onSubmit={handleSubmit(onSubmit)}>
       <Card className={style.card}>
         <div className={style.header}>
-        <Typography as={'h1'} className={style.typographyHead} variant={'h1'}>
-          Sign Up
-        </Typography>
+          <Typography as={'h1'} className={style.typographyHead} variant={'h1'}>
+            Sign Up
+          </Typography>
         </div>
         <div className={style.box}>
           <Input
             {...register('login', { required: true })}
+            className={style.inputStyle}
             disabled={false}
             error={errors.login?.message}
             label={'Login'}
             placeholder={'Login'}
             type={'text'}
-            className={style.inputStyle}
           />
           <Input
             {...register('password', { required: true })}
+            className={style.inputStyle}
             disabled={false}
             error={errors.password?.message}
             label={'Password'}
             placeholder={'Password'}
             type={'password'}
-            className={style.inputStyle}
           />
           <Input
             {...register('confirmPassword', { required: true })}
+            className={style.inputStyle}
             disabled={false}
             error={errors.password?.message}
             label={'Confirm Password'}
             placeholder={'Password'}
             type={'password'}
-            className={style.inputStyle}
           />
         </div>
 
-        <Button fullWidth>
-          Submit
-        </Button>
+        <Button fullWidth>Submit</Button>
         <div className={style.footer}>
-          <Typography as={'label'} variant={"body2"} className={style.typographyFooterTitle}>Already have an account?</Typography>
+          <Typography as={'label'} className={style.typographyFooterTitle} variant={'body2'}>
+            Already have an account?
+          </Typography>
           <Typography className={style.typographyFooterSubtitle} variant={'link1'}>
             Sign In
           </Typography>
