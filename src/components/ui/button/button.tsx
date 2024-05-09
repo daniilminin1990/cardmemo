45280c1df248959014c92ac60a4704a493e4bbf1
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2024-05-10 02:31:54 +0300

    fix: fixing forwardRef

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,4 +1,11 @@
-import { ComponentPropsWithoutRef, ElementType, ReactNode, forwardRef } from 'react'
+import {
+  ComponentPropsWithoutRef,
+  ElementRef,
+  ElementType,
+  ForwardedRef,
+  ReactNode,
+  forwardRef,
+} from 'react'
 
 import s from './button.module.scss'
 
@@ -10,7 +17,10 @@ export type ButtonProps<T extends ElementType = 'button'> = {
   variant?: 'primary' | 'secondary'
 } & ComponentPropsWithoutRef<T>
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
+export const ButtonPolymorph = <T extends ElementType = 'button'>(
+  props: ButtonProps<T>,
+  ref: any
+) => {
   const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
 
   return (
@@ -20,4 +30,11 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) =>
       {...rest}
     />
   )
-})
+}
+
+export const Button = forwardRef(ButtonPolymorph) as <T extends ElementType = 'button'>(
+  props: {
+    ref?: ForwardedRef<ElementRef<T>>
+  } & ButtonProps<T> &
+    Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
+) => ReturnType<typeof ButtonPolymorph>
