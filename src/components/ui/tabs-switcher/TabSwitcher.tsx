0d8ccf9702839bc167ff0490a99f20e008ed0d7c
Daniil <daniilminin1990@yandex.ru>
2024-05-13 18:47:07 +0500

    fixed PaginationWithSelect and TabSwitcher

--- src/components/ui/tabs-switcher/TabSwitcher.tsx
+++ src/components/ui/tabs-switcher/TabSwitcher.tsx
@@ -1,3 +1,4 @@
+import Typography from '@/components/ui/Typography/Typography'
 import * as Tabs from '@radix-ui/react-tabs'
 import { clsx } from 'clsx'
 
@@ -20,7 +21,9 @@ type Props = {
 export const TabSwitcher = ({ className, label, onValueChange, tabs, value }: Props) => {
   return (
     <div className={clsx(s.div, className)}>
-      {label}
+      <Typography as={'label'} variant={'body2'}>
+        {label}
+      </Typography>
       <Tabs.Root className={s.root} onValueChange={onValueChange} value={value}>
         <Tabs.List className={s.tabList}>
           {tabs.map(t => (
