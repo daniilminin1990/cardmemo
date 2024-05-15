72cfa2aed2340ef867b92bb954db99f6ccee6779
BladeDancer <69kir969@mail.ru>
2024-05-15 22:36:40 +0300

    remove corrupted stories

--- src/components/ui/table/table.stories.tsx
+++ src/components/ui/table/table.stories.tsx
@@ -2,9 +2,6 @@ import { Provider } from 'react-redux'
 import { MemoryRouter } from 'react-router-dom'
 
 import Typography from '@/components/ui/Typography/Typography'
-import { Card } from '@/components/ui/card'
-import { Decks } from '@/components/ui/table/decks/Decks'
-import { Deck } from '@/components/ui/table/decks/deck/Deck'
 import { store } from '@/services/Store'
 
 import s from './table.module.scss'
@@ -21,147 +18,6 @@ const meta = {
 }
 
 export default meta
-
-export const DecksTable = () => {
-  const data = [
-    {
-      author: { id: '1', name: 'BD' },
-      cardsCount: 1,
-      cover: null,
-      created: '2024-05-15T15:06:06.425Z',
-      id: '1',
-      isPrivate: false,
-      name: 'Hello World',
-      updated: '2024-05-05T15:06:06.425Z',
-      userId: '1',
-    },
-    {
-      author: { id: '2', name: 'Other' },
-      cardsCount: 3,
-      cover: null,
-      created: '2024-05-15T15:06:06.425Z',
-      id: '2',
-      isPrivate: false,
-      name: 'Hello World',
-      updated: '2024-05-12T15:06:06.425Z',
-      userId: '1',
-    },
-    {
-      author: { id: '3', name: 'Other' },
-      cardsCount: 0,
-      cover: null,
-      created: '2024-05-15T15:06:06.425Z',
-      id: '3',
-      isPrivate: false,
-      name: 'Hello World',
-      updated: '2024-05-15T15:06:06.425Z',
-      userId: '1',
-    },
-    {
-      author: { id: '1', name: 'BD' },
-      cardsCount: 0,
-      cover: null,
-      created: '2024-05-15T15:06:06.425Z',
-      id: '1',
-      isPrivate: false,
-      name: 'Hello World',
-      updated: '2024-05-15T15:06:06.425Z',
-      userId: '1',
-    },
-  ]
-
-  return (
-    <MemoryRouter>
-      <Decks
-        Component={Deck}
-        data={data}
-        headersName={[
-          { key: 'name', title: 'Name' },
-          { key: 'cardsCount', title: 'Cards' },
-          { key: 'updated', title: 'Last Updated' },
-          { key: 'created', title: 'Created by' },
-        ]}
-        sortedColumn={'updated'}
-      />
-    </MemoryRouter>
-  )
-}
-export const CardsTable = () => {
-  const data = [
-    {
-      answer: 'ввв',
-      answerImg:
-        'https://staging-it-incubator.s3.eu-central-1.amazonaws.com/flashcards/Image/98795c6d-403f-4bfb-ac7d-a20d30da7c1b_user.png',
-      answerVideo: null,
-      created: '2024-03-18T12:17:12.850Z',
-      deckId: 'cltsu3okg000sx22gmwec2kly',
-      grade: 5,
-      id: 'cltwwuqaa013mvq2gwnc93n7g',
-      question: 'ghghkghk',
-      questionImg: null,
-      questionVideo: null,
-      shots: 9,
-      updated: '2024-05-11T17:14:21.696Z',
-      userId: 'df6760fa-5ae1-46ef-916e-85f670d7b903',
-    },
-    {
-      answer: 'ввв',
-      answerImg:
-        'https://staging-it-incubator.s3.eu-central-1.amazonaws.com/flashcards/Image/98795c6d-403f-4bfb-ac7d-a20d30da7c1b_user.png',
-      answerVideo: null,
-      created: '2024-03-18T12:17:12.850Z',
-      deckId: 'cltsu3okg000sx22gmwec2kly',
-      grade: 5,
-      id: 'cltwwuqaa013mvq2gwnc93n7g',
-      question: 'ghghkghk',
-      questionImg: null,
-      questionVideo: null,
-      shots: 9,
-      updated: '2024-05-11T17:14:21.696Z',
-      userId: 'df6760fa-5ae1-46ef-916e-85f670d7b903',
-    },
-    {
-      answer: 'ввв',
-      answerImg:
-        'https://staging-it-incubator.s3.eu-central-1.amazonaws.com/flashcards/Image/98795c6d-403f-4bfb-ac7d-a20d30da7c1b_user.png',
-      answerVideo: null,
-      created: '2024-03-18T12:17:12.850Z',
-      deckId: 'cltsu3okg000sx22gmwec2kly',
-      grade: 5,
-      id: 'cltwwuqaa013mvq2gwnc93n7g',
-      question: 'ghghkghk',
-      questionImg: null,
-      questionVideo: null,
-      shots: 9,
-      updated: '2024-05-11T17:14:21.696Z',
-      userId: 'df6760fa-5ae1-46ef-916e-85f670d7b903',
-    },
-  ]
-
-  return (
-    <MemoryRouter>
-      <Decks
-        Component={Card}
-        data={data}
-        headersName={[
-          { key: 'question', title: 'Question' },
-          { key: 'answer', title: 'Answer' },
-          { key: 'updated', title: 'Last Updated' },
-          { key: 'grade', title: 'Grade' },
-        ]}
-        sortedColumn={'updated'}
-      />
-    </MemoryRouter>
-  )
-}
-export const WithoutContent = () => {
-  return (
-    <Typography as={'div'} className={s.empty} variant={'body1'}>
-      No content with these terms...
-    </Typography>
-  )
-}
-
 export const DefaultTable = () => {
   const headersName = [
     { key: 'name', title: 'Name' },
@@ -187,12 +43,17 @@ export const DefaultTable = () => {
         </Table.Head>
         <Table.Body>
           <Table.Row>
-            <Table.Cell className={`empty`} colSpan={100}>
-              No content with these terms...
-            </Table.Cell>
+            <Table.Cell>The place for rows...</Table.Cell>
           </Table.Row>
         </Table.Body>
       </Table.Root>
     </MemoryRouter>
+  )
+}
+export const WithoutContent = () => {
+  return (
+    <Typography as={'div'} className={s.empty} variant={'body1'}>
+      No content with these terms...
+    </Typography>
   )
 }
