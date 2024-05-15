03e9518f42e137d35c2b5a19134102db32c43611
BladeDancer <69kir969@mail.ru>
2024-05-15 21:38:10 +0300

    add "@reduxjs/toolkit": "^2.2.4" ; "react-redux": "^9.1.2",

---
+++ src/services/Store.ts
@@ -0,0 +1,12 @@
+import { flashcardsApi } from '@/services/flashCardsApi'
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(flashcardsApi.middleware),
+  reducer: {
+    [flashcardsApi.reducerPath]: flashcardsApi.reducer,
+  },
+})
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
