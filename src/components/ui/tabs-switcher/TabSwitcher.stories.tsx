cc04f0d525778f744540039feb036c7dd14cb05f
Daniil <129886977+daniilminin1990@users.noreply.github.com>
2024-05-17 01:13:56 +0500

    Fixed TasbSwitch styles; Refactor Select ItemsPerPageHandler --> onValueChange Deleted trash components
    * refactor tabsSwitcher styles and stories. On Tab press it focuses on buttons
    
    * delete trash components
    
    * refactor Select ItemsPerPageHandler --> onValueChange

--- src/components/ui/tabs-switcher/TabSwitcher.stories.tsx
+++ src/components/ui/tabs-switcher/TabSwitcher.stories.tsx
@@ -12,10 +12,17 @@ const meta = {
 
 export default meta
 
-const tabs = [
+const tabsDefault = [
   { text: 'Select1', value: 'Select1' },
   { text: 'Select2', value: 'Select2' },
   { text: 'Select3', value: 'Select3' },
+  { text: 'Select4', value: 'Select4' },
+]
+
+const tabsDisabled = [
+  { disabled: true, text: 'Select1', value: 'Select1' },
+  { disabled: true, text: 'Select2', value: 'Select2' },
+  { disabled: true, text: 'Select3', value: 'Select3' },
   { disabled: true, text: 'Select4', value: 'Select4' },
 ]
 
@@ -33,7 +40,30 @@ export const Default = {
         <TabSwitcher
           label={label}
           onValueChange={value => setValue(value)}
-          tabs={tabs}
+          tabs={tabsDefault}
+          value={value}
+        />
+        <p style={{ marginTop: '36px' }}>{value}</p>
+      </div>
+    )
+  },
+}
+
+export const Disabled = {
+  render: () => {
+    const [value, setValue] = useState('myCards')
+    const label = 'Show tabs'
+
+    return (
+      <div
+        style={{
+          width: '230px',
+        }}
+      >
+        <TabSwitcher
+          label={label}
+          onValueChange={value => setValue(value)}
+          tabs={tabsDisabled}
           value={value}
         />
         <p style={{ marginTop: '36px' }}>{value}</p>
