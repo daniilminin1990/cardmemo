ced096c31f02b2a7c0ca0ef2f5c367d222835b2b
ANDREI SHALOMITSKII <andreyshalomitsky@gmail.com>
2024-05-16 17:53:51 +0500

    delete width styles in Card, added props className, styles in Modal, corrected checkbox in SignIn
    * added Card
    
    * added Modal
    
    * created forwardRef Modal
    
    * changed forwardRef Modal
    
    * changed App
    
    * merge main in modal
    
    * delete padding in Card, change stories in Card and Modal
    
    * change signUp
    
    * created <BrowserRouter> in sign-in.stories
    
    * delete width styles in Card, added props className, styles in Modal, corrected checkbox in SignIn

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -1,24 +1,27 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { CSSProperties, ComponentPropsWithoutRef } from 'react'
 
 import Typography from '@/components/ui/Typography/Typography'
 import { Card } from '@/components/ui/card'
 import * as Dialog from '@radix-ui/react-dialog'
 import { Cross2Icon } from '@radix-ui/react-icons'
+import { clsx } from 'clsx'
 
 import s from './modal.module.scss'
 
 export type Props = {
+  className?: string
   onOpenChange: (value: boolean) => void
   open: boolean
+  style?: CSSProperties
   title?: string
 } & Omit<ComponentPropsWithoutRef<typeof Dialog.Dialog>, 'onOpenChange' | 'open'>
 
-export const Modal = ({ children, title, ...props }: Props) => (
+export const Modal = ({ children, className, style, title, ...props }: Props) => (
   <Dialog.Root {...props}>
     <Dialog.Portal>
       <Dialog.Overlay className={s.DialogOverlay} />
-      <Dialog.Content className={s.DialogContent}>
-        <Card>
+      <Dialog.Content className={clsx(s.DialogContent)}>
+        <Card className={className} style={style}>
           <div className={s.header}>
             <Typography as={'h2'} variant={'h2'}>
               {title}
