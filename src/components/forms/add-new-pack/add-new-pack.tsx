7913464eb487e143c2541eec28a4b74914e884e3
andreyshalomitsky <andreyshalomitsky@gmail.com>
2024-05-19 08:25:57 +0500

    created addNewPack

---
+++ src/components/forms/add-new-pack/add-new-pack.tsx
@@ -0,0 +1,118 @@
+import { Controller } from 'react-hook-form'
+
+import {
+  AddNewPackFormType,
+  useAddNewPackForm,
+} from '@/components/forms/add-new-pack/use-add-new-pack'
+import { useUploadImg } from '@/components/forms/add-new-pack/use-upload-img'
+import { Button } from '@/components/ui/button'
+import Checkbox from '@/components/ui/checkbox/checkbox'
+import { FormPreviewFileUploader } from '@/components/ui/form/form-preview-file-uploader/form-preview-file-uploader'
+import { FormTextfield } from '@/components/ui/form/form-textfield'
+import { DevTool } from '@hookform/devtools'
+
+import s from './add-new-pack.module.scss'
+
+type AddNewPackFormDV = {
+  cover: null | string
+} & AddNewPackFormType
+
+type Props = {
+  defaultValues?: AddNewPackFormDV
+  onSubmit: (data: FormData) => void
+  setOpenModal: (open: boolean) => void
+}
+
+export const AddNewPack = ({ defaultValues, onSubmit, setOpenModal }: Props) => {
+  const values: AddNewPackFormType = {
+    isPrivatePack: defaultValues?.isPrivatePack || false,
+    packName: defaultValues?.packName || '',
+  }
+
+  const { control, getFieldState, handleSubmit, resetField, setValue, trigger, watch } =
+    useAddNewPackForm(values)
+
+  const {
+    coverError: packCoverError,
+    deleteCoverHandler: packDeleteCoverHandler,
+    downloaded: packDownloaded,
+    extraActions: packExtraActions,
+  } = useUploadImg({
+    getFieldState,
+    resetField,
+    setValue,
+    trigger,
+    watch,
+  })
+
+  const fileAnswerIsDirty = getFieldState('coverPack').isDirty
+  const fileAnswer = watch('coverPack')
+
+  const sendHandler = (data: AddNewPackFormType) => {
+    const form = new FormData()
+
+    form.append('packName', data.packName)
+
+    form.append('isPrivatePack', `${data.isPrivatePack}`)
+
+    if (fileAnswer === null) {
+      form.append('coverPack', '')
+    } else if (fileAnswerIsDirty && data.coverPack) {
+      form.append('coverPack', data.coverPack)
+    }
+
+    onSubmit(form)
+
+    //for test
+    for (const pair of form.entries()) {
+      console.log(pair[0] + ': ' + pair[1])
+    }
+
+    setOpenModal(false)
+  }
+
+  return (
+    <>
+      {import.meta.env.DEV && <DevTool control={control} />}
+      <form className={s.form} onSubmit={handleSubmit(sendHandler)}>
+        <div className={s.box}>
+          <FormTextfield
+            className={s.inputStyle}
+            control={control}
+            label={'Name Pack'}
+            name={'packName'}
+            placeholder={'Name'}
+            type={'text'}
+          />
+          <FormPreviewFileUploader
+            className={s.uploadImg}
+            control={control}
+            deleteCoverHandler={() => packDeleteCoverHandler('coverPack')}
+            errorMessage={packCoverError}
+            extraActions={() => packExtraActions('coverPack')}
+            name={'coverPack'}
+            preview={packDownloaded}
+          />
+          <Controller
+            control={control}
+            defaultValue={false}
+            name={'isPrivatePack'}
+            render={({ field: { onChange, value } }) => (
+              <div className={s.checkbox}>
+                <Checkbox checked={value} label={'Private Pack'} onCheckedChange={onChange} />
+              </div>
+            )}
+          />
+        </div>
+        <div className={s.footer}>
+          <Button onClick={() => setOpenModal(false)} variant={'secondary'}>
+            Cancel
+          </Button>
+          <Button type={'submit'} variant={'primary'}>
+            Add New Pack
+          </Button>
+        </div>
+      </form>
+    </>
+  )
+}
---
+++ src/components/forms/add-new-pack/use-upload-img.ts
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+type FieldError = {
+  message?: string
+}
+
+type name = 'coverPack' | 'isPrivatePack' | 'packName'
+
+type UploadImgType = {
+  getFieldState: (name: name) => { error?: FieldError }
+  resetField: (name: name) => void
+  setValue: (name: name, value: any) => void
+  trigger: (name: name) => Promise<boolean>
+  watch: (name: name) => any
+}
+
+export const useUploadImg = ({
+  getFieldState,
+  resetField,
+  setValue,
+  trigger,
+  watch,
+}: UploadImgType) => {
+  const [downloaded, setDownloaded] = useState<null | string>(null)
+  const [coverError, setCoverError] = useState<null | string>(null)
+
+  const deleteCoverHandler = (cover: name) => {
+    if (coverError) {
+      setCoverError(null)
+    }
+    // toast.warning('You deleted cover', { containerId: 'modal' })
+    setValue(cover, null)
+    setDownloaded(null)
+  }
+
+  const extraActions = async (cover: name) => {
+    const success = await trigger(cover)
+    const { error } = getFieldState(cover)
+    const file = watch(cover)
+
+    if (!success && error?.message) {
+      // toast.error(error.message, { containerId: 'modal' })
+      setCoverError(error.message)
+      resetField(cover)
+    }
+
+    if (file) {
+      const badCase = null
+      const img = success ? URL.createObjectURL(file) : badCase
+
+      setDownloaded(img)
+
+      if (coverError && !error?.message) {
+        setCoverError(null)
+      }
+    }
+  }
+
+  return { coverError, deleteCoverHandler, downloaded, extraActions }
+}
