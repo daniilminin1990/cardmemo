0ed050dd448e163e87ef85b98090b5d08bdaaac1
Daniil <daniilminin1990@yandex.ru>
2024-05-22 03:57:47 +0500

    add DataFiller to upload images and data to card. Conole log works. Need to make scroll

---
+++ src/pagesMinin/ModalsForTable/ModalEditCard/DataFiller/DataFiller.tsx
@@ -0,0 +1,89 @@
+import { ChangeEvent, useEffect, useRef, useState } from 'react'
+import { Control } from 'react-hook-form'
+
+import ImageOutline from '@/assets/icons/svg/ImageOutline'
+import Input from '@/components/ui/Input/Input'
+import Typography from '@/components/ui/Typography/Typography'
+import { Button } from '@/components/ui/button'
+import { FormTextfield } from '@/components/ui/form/form-textfield'
+import clsx from 'clsx'
+
+import s from './dataFiller.module.scss'
+
+import { Card } from '../../../../../services/decks/deck.types'
+
+type DataFillerProps = {
+  control: Control<any, any>
+  item?: Card
+  name: string
+  preview: null | string
+  setPreview: (value: null | string) => void
+  title: string
+}
+export const DataFiller = (props: DataFillerProps) => {
+  const { control, item, name, preview, setPreview, title } = props
+  // const [updateDeck] = useUpdateDeckMutation()
+  const [cover, setCover] = useState<File | null>(null)
+  const refInputImg = useRef<HTMLInputElement>(null)
+  // Генерируем ссылку на загружаемый файл и сэтаем в preview, который будем отображать, и очищаем после сэта хэш
+
+  useEffect(() => {
+    if (cover) {
+      const newPreviewQuestion = URL.createObjectURL(cover)
+
+      if (preview) {
+        URL.revokeObjectURL(preview)
+      }
+
+      setPreview(newPreviewQuestion)
+
+      return () => URL.revokeObjectURL(newPreviewQuestion)
+    }
+  }, [cover])
+
+  const handleInputImg = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log('e.target.files', e.target.files)
+    // if (e.target.files !== null && e.target.files.length > 0) {
+    //   setPreview(URL.createObjectURL(e.target.files[0]))
+    // }
+    setCover(e.target.files?.[0] ?? null)
+  }
+  const hanldeSubmitImg = () => {
+    refInputImg?.current?.click()
+  }
+
+  return (
+    <div>
+      <Typography variant={'subtitle2'}>{title}</Typography>
+      <FormTextfield
+        className={s.input}
+        control={control}
+        label={item ? 'Edit question' : title}
+        name={name}
+      />
+      {preview && (
+        <div className={s.imgWrapper}>
+          <img alt={'cover'} className={s.img} src={preview} />
+        </div>
+      )}
+      {item?.question && (
+        <Typography className={s.questionTxt} variant={'h1'}>
+          {item.question}
+        </Typography>
+      )}
+      <Button className={s.uploadImg} fullWidth onClick={hanldeSubmitImg} type={'button'}>
+        <ImageOutline className={s.icon} />
+        <Typography variant={'subtitle2'}>{preview ? 'Change cover' : 'Upload Image'}</Typography>
+        {/*<Input className={s.inputImg} id={'upload-photo'} name={'photo'} type={'file'} />*/}
+        <Input
+          accept={'image/*'}
+          className={s.inputImg}
+          name={'cover'}
+          onChange={handleInputImg}
+          ref={refInputImg}
+          type={'file'}
+        />
+      </Button>
+    </div>
+  )
+}
---
+++ src/pagesMinin/ModalsForTable/ModalEditCard/ModalEditCard.tsx
@@ -0,0 +1,94 @@
+import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+import Typography from '@/components/ui/Typography/Typography'
+import { Button } from '@/components/ui/button'
+import { Modal } from '@/components/ui/modal/modal'
+import { DataFiller } from '@/pagesMinin/ModalsForTable/ModalEditCard/DataFiller/DataFiller'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+import s from './modalEditCardMinin.module.scss'
+
+import { Card } from '../../../../services/decks/deck.types'
+
+type ModalAddEditProps = {
+  item?: Card
+  open: boolean
+  setOpen: (value: boolean) => void
+}
+
+export const ModalEditCard = (props: ModalAddEditProps) => {
+  const { item, open, setOpen } = props
+  const initPreviewQuestion = item ? item.questionImg ?? null : ''
+  const initPreviewAnswer = item ? item.answerImg ?? null : ''
+  const [previewQuestion, setPreviewQuestion] = useState<null | string>(initPreviewQuestion)
+  const [previewAnswer, setPreviewAnswer] = useState<null | string>(initPreviewAnswer)
+  const schema = z.object({
+    answer: item ? z.string() : z.string().min(3).max(1000),
+    question: item ? z.string() : z.string().min(3).max(1000),
+  })
+
+  type FormValues = z.infer<typeof schema>
+  const { control, handleSubmit } = useForm<FormValues>({
+    defaultValues: { answer: '', question: '' },
+    resolver: zodResolver(schema),
+  })
+
+  const onSubmit: SubmitHandler<FormValues> = data => {
+    // item && updateCard({ ...data, coverQuestion, id: item.id })
+    console.log({ answerImg: previewAnswer, data: { ...data }, questionImg: previewQuestion })
+    setOpen(false)
+  }
+
+  const handleOnClose = () => {
+    if (item) {
+      setPreviewQuestion(item.questionImg || null)
+      setPreviewAnswer(item.answerImg || null)
+    } else {
+      setPreviewQuestion(null)
+      setPreviewAnswer(null)
+    }
+    setOpen(false)
+  }
+
+  return (
+    <Modal
+      className={s.customClass}
+      onOpenChange={handleOnClose}
+      open={open}
+      title={item ? 'Update Card' : 'Add New Card'}
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={s.body}>
+          <DataFiller
+            control={control}
+            item={item}
+            name={'question'}
+            preview={previewQuestion}
+            setPreview={setPreviewQuestion}
+            title={'Question'}
+          />
+          <DataFiller
+            control={control}
+            item={item}
+            name={'answer'}
+            preview={previewAnswer}
+            setPreview={setPreviewAnswer}
+            title={'Answer'}
+          />
+        </div>
+        <div className={s.footer}>
+          <Button onClick={handleOnClose} type={'button'} variant={'secondary'}>
+            <Typography variant={'subtitle2'}>Cancel</Typography>
+          </Button>
+          <Button
+          // Не обязательное говно, т.к. по умолчанию onSubmit
+          >
+            <Typography variant={'subtitle2'}>{item ? 'Save changes' : 'Create Card'}</Typography>
+          </Button>
+        </div>
+      </form>
+    </Modal>
+  )
+}
