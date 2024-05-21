7389ab0b0a84e3f246c4f5f2c161b657c04f0e0b
Daniil <daniilminin1990@yandex.ru>
2024-05-21 22:06:59 +0500

    refactor routes, simplified

--- src/router.tsx
+++ src/router.tsx
@@ -26,39 +26,84 @@ const privateRoutes: RouteObject[] = [
         path: '/decks/:deckId',
       },
     ],
-    element: <Container />,
+    element: <Outlet />,
   },
 ]
-
-const publicRoutes: RouteObject[] = [
-  {
-    element: <Login />,
-    path: '/login',
-  },
-]
-
-const router = createBrowserRouter([
-  {
-    children: privateRoutes,
-    element: <PrivateRoutes />,
-  },
-  ...publicRoutes,
-])
-
-export function Router() {
-  return <RouterProvider router={router} />
-}
-
-function Container() {
-  return <Outlet />
-}
-
-function Login() {
-  return <h1>Залогинься, чмо</h1>
-}
 
 function PrivateRoutes() {
   const isAuthenticated = true
 
   return isAuthenticated ? <Outlet /> : <Navigate to={'/login'} />
 }
+
+export const router = createBrowserRouter([
+  {
+    children: privateRoutes,
+    element: <PrivateRoutes />, // <Outlet /> : <Navigate to={'/login'} />
+  },
+  {
+    element: <h1>Залогинься, чмо</h1>,
+    path: '/login',
+  },
+])
+
+export function Router() {
+  return <RouterProvider router={router} />
+}
+
+// export function Router() {
+//   return <RouterProvider router={router} />
+// }
+
+// const privateRoutes: RouteObject[] = [
+//   {
+//     children: [
+//       {
+//         element: <DecksMininPage />,
+//         path: '/',
+//       },
+//       {
+//         element: <TestDecks />,
+//         path: '/cards',
+//       },
+//       {
+//         element: <DeckCardsPage />,
+//         path: '/decks/:deckId',
+//       },
+//     ],
+//     element: <Container />,
+//   },
+// ]
+//
+// const publicRoutes: RouteObject[] = [
+//   {
+//     element: <Login />,
+//     path: '/login',
+//   },
+// ]
+//
+// const router = createBrowserRouter([
+//   {
+//     children: privateRoutes,
+//     element: <PrivateRoutes />,
+//   },
+//   ...publicRoutes,
+// ])
+//
+// export function Router() {
+//   return <RouterProvider router={router} />
+// }
+//
+// function Container() {
+//   return <Outlet />
+// }
+//
+// function Login() {
+//   return <h1>Залогинься, чмо</h1>
+// }
+//
+// function PrivateRoutes() {
+//   const isAuthenticated = true
+//
+//   return isAuthenticated ? <Outlet /> : <Navigate to={'/login'} />
+// }
