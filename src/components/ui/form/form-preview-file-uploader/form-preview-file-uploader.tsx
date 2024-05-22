24452d0dd81d50c0f5ba2c05c945dd4d7251e997
ANDREI SHALOMITSKII <andreyshalomitsky@gmail.com>
2024-05-22 13:36:38 +0500

    created addNewCard, common/utils/zod-schemas, ui/file-uploader, form/form-preview-file-uploader and changed Typography 'Forgot Password?' and 'Don't have an account?' on 'button'
    * added Card
    
    * added Modal
    
    * created forwardRef Modal
    
    * changed forwardRef Modal
    
    * changed App
    
    * merge main in modal
    
    * delete padding in Card, change stories in Card and Modal
    
    * change signUp
    
    * created <BrowserRouter> in sign-in.stories
    
    * delete width styles in Card, added props className, styles in Modal, corrected checkbox in SignIn
    
    * added className in Modal.stories
    
    * created addNewCard, common/utils/zod-schemas, ui/file-uploader, form/form-preview-file-uploader
    
    * created addNewCard
    
    * created addNewPack
    
    * updated useUploadImg
    
    * changed Typography 'Forgot Password?' and 'Don't have an account?' on 'button'
    
    * clean branch modal

---
+++ src/components/ui/form/form-preview-file-uploader/form-preview-file-uploader.tsx
@@ -0,0 +1,86 @@
+import { useState } from 'react'
+import { Control, FieldPath, FieldValues } from 'react-hook-form'
+
+import ImageOutline from '@/assets/icons/svg/ImageOutline'
+import Trash from '@/assets/icons/svg/Trash'
+import defImg from '@/assets/img/defaultCard.jpg'
+import Typography from '@/components/ui/Typography/Typography'
+import { Button } from '@/components/ui/button'
+import { FormFileUploader } from '@/components/ui/form/form-file-uploader'
+import { clsx } from 'clsx'
+
+import s from './form-preview-file-uploader.module.scss'
+
+type Props<T extends FieldValues> = {
+  className?: string
+  control: Control<T>
+  deleteCoverHandler: () => void
+  displayInlineError?: boolean
+  errorMessage: null | string
+  extraActions?: () => void
+  name: FieldPath<T>
+  preview: null | string
+}
+
+export const FormPreviewFileUploader = <T extends FieldValues>(props: Props<T>) => {
+  const {
+    control,
+    deleteCoverHandler,
+    displayInlineError = false,
+    errorMessage,
+    extraActions,
+    name,
+    preview,
+  } = props
+
+  const [open, setOpen] = useState(false)
+
+  const imgClasses = clsx(s.image, preview && s.hover, open && s.open)
+
+  const onClickHandler = () => {
+    if (preview) {
+      setOpen(prevState => !prevState)
+    }
+  }
+
+  const deleteHandler = () => {
+    deleteCoverHandler()
+    if (open) {
+      setOpen(false)
+    }
+  }
+
+  return (
+    <div className={clsx(s.root, props.className)}>
+      <img alt={'img'} className={imgClasses} onClick={onClickHandler} src={preview ?? defImg} />
+      {displayInlineError && errorMessage && (
+        <Typography className={s.error} variant={'caption'}>
+          {errorMessage}
+        </Typography>
+      )}
+      <div className={s.previewControls}>
+        {preview && (
+          <Button
+            className={s.uploadImg}
+            fullWidth
+            onClick={deleteHandler}
+            type={'button'}
+            variant={'secondary'}
+          >
+            <Trash className={s.imgIcon} /> Delete Cover
+          </Button>
+        )}
+        <FormFileUploader
+          control={control}
+          extraActions={extraActions}
+          fullWidth={!preview}
+          name={name}
+          variant={'secondary'}
+        >
+          <ImageOutline className={s.imgIcon} />
+          Change Cover
+        </FormFileUploader>
+      </div>
+    </div>
+  )
+}
