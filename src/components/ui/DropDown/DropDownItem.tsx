8372b3ec4ef8b5bd513dabbcb16cd4cc65553a23
Daniil <129886977+daniilminin1990@users.noreply.github.com>
2024-05-26 19:43:03 +0500

    refactor DropDown styles on module.scss, DropDownItm passed as and handleOnClick, now able pass this props,  and wrapped Typography with Button and this handleOnClick refactor SignIn -- wrapped Typo with Button type button
    * refactor DropDown styles on module.scss, DropDownItm passed handleOnClick now able give this prop and wrapped Typography with Button and this handleOnClick
    refactor SignIn -- wrapped Typo with Button type button
    
    * refactor DropDownItem with props as and handleOnClick for Button

--- src/components/ui/DropDown/DropDownItem.tsx
+++ src/components/ui/DropDown/DropDownItem.tsx
@@ -1,21 +1,38 @@
+import { ElementType } from 'react'
+
 import Typography from '@/components/ui/Typography/Typography'
+import { Button } from '@/components/ui/button'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
+import s from './DropDown.module.scss'
+
 type DropDownItemProps = {
+  //! Добавил as, чтобы кидать в Button
+  as?: ElementType
+  //! Добавил handleClick
+  handleOnClick?: () => void
   icon: string
   text: string
 }
 
 const DropDownItem = (props: DropDownItemProps) => {
-  const { icon, text } = props
+  const { as, handleOnClick, icon, text } = props
 
   return (
-    <div className={'DropdownMenuItemBox'}>
-      <DropdownMenu.Item className={'DropdownMenuItem'}>
-        <img alt={''} src={icon} />
-        <Typography className={'dropdownText'} variant={'caption'}>
-          {text}
-        </Typography>
+    <div className={s.DropdownMenuItemBox}>
+      <DropdownMenu.Item className={s.DropdownMenuItem}>
+        {/*! Добавил Button*/}
+        <Button
+          as={as}
+          href={'/'}
+          onClick={handleOnClick}
+          style={{ alignItems: 'center', all: 'unset', display: 'flex' }}
+        >
+          <img alt={''} src={icon} />
+          <Typography className={s.dropdownText} variant={'caption'}>
+            {text}
+          </Typography>
+        </Button>
       </DropdownMenu.Item>
     </div>
   )
