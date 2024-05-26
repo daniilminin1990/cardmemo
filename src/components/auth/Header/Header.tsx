b05cba387173c1a601d2c513df50c497ca347f2f
Daniil <129886977+daniilminin1990@users.noreply.github.com>
2024-05-26 21:15:40 +0500

    refactor DropDownItem props remove as, refactor styles refactor Header - gave links
    * refactor DropDown styles on module.scss, DropDownItm passed handleOnClick now able give this prop and wrapped Typography with Button and this handleOnClick
    refactor SignIn -- wrapped Typo with Button type button
    
    * refactor DropDownItem with props as and handleOnClick for Button
    
    * refactor DropDownItem with props href if it will be a link
    
    * refactor DropDownItem props remove as, refactor styles
    refactor Header - gave links

--- src/components/auth/Header/Header.tsx
+++ src/components/auth/Header/Header.tsx
@@ -5,6 +5,7 @@ import DropdownMenuDemo from '@/components/ui/DropDown/DropDown'
 import DropDownItem from '@/components/ui/DropDown/DropDownItem'
 import Typography from '@/components/ui/Typography/Typography'
 import { Button } from '@/components/ui/button'
+import { path } from '@/router/path'
 import { useLogoutMutation } from '@/services/auth/auth.service'
 import { MeResponse } from '@/services/auth/auth.types'
 
@@ -26,7 +27,7 @@ const Header = ({ data }: HeaderProps) => {
     <div className={style.box}>
       <div className={style.boxImg}>
         {/*! Сделал ссылку на главную страницу*/}
-        <Typography as={'a'} href={'/'} variant={'body2'}>
+        <Typography as={'a'} href={`${path.decks}`} variant={'body2'}>
           <img alt={'logo'} className={style.img} src={logo} />
         </Typography>
       </div>
@@ -34,11 +35,10 @@ const Header = ({ data }: HeaderProps) => {
         <div className={style.dropDown}>
           <div className={style.text}>{data.name}</div>
           <DropdownMenuDemo data={data} icon={ellipseIcon} type={'head'}>
-            <DropDownItem icon={headerIcon} text={'My Profile'} />
+            <DropDownItem href={`${path.profile}`} icon={headerIcon} text={'My Profile'} />
             <DropDownItem
-              as={'a'}
               handleOnClick={logoutHandler}
-              href={'/'}
+              href={`${path.login}`}
               icon={headerIcon1}
               text={'Sign Out'}
             />
--- src/components/ui/DropDown/DropDownItem.tsx
+++ src/components/ui/DropDown/DropDownItem.tsx
@@ -1,15 +1,14 @@
-import { ElementType } from 'react'
+import { Link } from 'react-router-dom'
 
 import Typography from '@/components/ui/Typography/Typography'
 import { Button } from '@/components/ui/button'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
 import s from './DropDown.module.scss'
+import style from '@/components/auth/Header/Header.module.scss'
 
 type DropDownItemProps = {
-  //! Добавил as, чтобы кидать в Button
-  as?: ElementType
-  //! Добавил handleClick
+  //! Добавил handleClick, href
   handleOnClick?: () => void
   href?: string
   icon: string
@@ -17,26 +16,40 @@ type DropDownItemProps = {
 }
 
 const DropDownItem = (props: DropDownItemProps) => {
-  const { as, handleOnClick, href, icon, text } = props
+  const { handleOnClick, href, icon, text } = props
 
   return (
     <div className={s.DropdownMenuItemBox}>
       <DropdownMenu.Item className={s.DropdownMenuItem}>
-        {/*! Добавил Button*/}
-        <Button
-          as={as}
-          href={href}
-          onClick={handleOnClick}
-          style={{ alignItems: 'center', all: 'unset', display: 'flex' }}
-        >
-          <img alt={''} src={icon} />
-          <Typography className={s.dropdownText} variant={'caption'}>
-            {text}
-          </Typography>
-        </Button>
+        {/*! Добавил Button, href, Link*/}
+        {href ? (
+          <Link className={style.Link} to={href}>
+            {/*! Вынес кнопку с Typography в отдельную компоненту*/}
+            <DDButton handleOnClick={handleOnClick} icon={icon} text={text} />
+          </Link>
+        ) : (
+          <>
+            <DDButton handleOnClick={handleOnClick} icon={icon} text={text} />
+          </>
+        )}
       </DropdownMenu.Item>
     </div>
   )
 }
 
+type DropDownButtonProps = {
+  handleOnClick?: () => void
+  icon: string
+  text: string
+}
+export const DDButton = ({ handleOnClick, icon, text }: DropDownButtonProps) => {
+  return (
+    <Button className={s.button} onClick={handleOnClick}>
+      <img alt={''} src={icon} />
+      <Typography className={s.dropdownText} variant={'caption'}>
+        {text}
+      </Typography>
+    </Button>
+  )
+}
 export default DropDownItem
