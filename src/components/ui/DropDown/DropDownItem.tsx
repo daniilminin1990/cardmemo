6723deb598af7b73650f539653d4e74be6356ef8
Daniil <daniilminin1990@yandex.ru>
2024-05-26 19:28:36 +0500

    refactor DropDown styles on module.scss, DropDownItm passed handleOnClick now able give this prop and wrapped Typography with Button and this handleOnClick refactor SignIn -- wrapped Typo with Button type button

--- src/components/ui/DropDown/DropDownItem.tsx
+++ src/components/ui/DropDown/DropDownItem.tsx
@@ -1,21 +1,31 @@
 import Typography from '@/components/ui/Typography/Typography'
+import { Button } from '@/components/ui/button'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
+import s from './DropDown.module.scss'
+
 type DropDownItemProps = {
+  handleOnClick?: () => void
   icon: string
   text: string
 }
 
 const DropDownItem = (props: DropDownItemProps) => {
-  const { icon, text } = props
+  const { handleOnClick, icon, text } = props
 
   return (
-    <div className={'DropdownMenuItemBox'}>
-      <DropdownMenu.Item className={'DropdownMenuItem'}>
-        <img alt={''} src={icon} />
-        <Typography className={'dropdownText'} variant={'caption'}>
-          {text}
-        </Typography>
+    <div className={s.DropdownMenuItemBox}>
+      <DropdownMenu.Item className={s.DropdownMenuItem}>
+        {/*! Добавил Button*/}
+        <Button
+          onClick={handleOnClick}
+          style={{ alignItems: 'center', all: 'unset', display: 'flex' }}
+        >
+          <img alt={''} src={icon} />
+          <Typography className={s.dropdownText} variant={'caption'}>
+            {text}
+          </Typography>
+        </Button>
       </DropdownMenu.Item>
     </div>
   )
