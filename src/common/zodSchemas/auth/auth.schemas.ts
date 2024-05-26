356b549c4b571d8ee88f481b89bfe73fa7cb5977
Kirill <112247196+kirabortz@users.noreply.github.com>
2024-05-27 00:18:30 +0300

    create CreateNewPassword (#138)
    * create CreateNewPassword , create SuccessModal , change server:3000 in vite.config

--- src/common/zodSchemas/auth/auth.schemas.ts
+++ src/common/zodSchemas/auth/auth.schemas.ts
@@ -6,3 +6,15 @@ export const PersonalInfoScheme = z.object({
 })
 
 export type PersonalInfoFormValue = z.infer<typeof PersonalInfoScheme>
+
+export const ResetPasswordSchema = z
+  .object({
+    confirmNewPassword: z.string().min(3),
+    newPassword: z.string().min(3),
+  })
+  .refine(data => data.newPassword === data.confirmNewPassword, {
+    message: "Passwords don't match",
+    path: ['confirmNewPassword'],
+  })
+
+export type ResetPasswordFormValues = z.infer<typeof ResetPasswordSchema>
