97584475d4f740727f8ff4356dbf2fa524b90b53
BladeDancer <69kir969@mail.ru>
2024-05-29 11:16:06 +0300

    Fix logout toast

--- src/common/consts/toastVariants.ts
+++ src/common/consts/toastVariants.ts
@@ -3,16 +3,16 @@ import { toast } from 'react-toastify'
 import { useThrottle } from '@/hooks/useThrottle'
 
 export const handleToastSuccess = (message: string, autoClose: number = 3000) => {
-  toast.success(message, { autoClose: autoClose })
+  toast.success(message, { autoClose })
 }
 export const handleToastError = (message: string, autoClose: number = 3000) => {
-  toast.error(message, { autoClose: autoClose })
+  toast.error(message, { autoClose })
 }
 export const handleToastWarning = (message: string, autoClose: number = 3000) => {
-  toast.warning(message, { autoClose: autoClose })
+  toast.warning(message, { autoClose })
 }
 export const handleToastInfo = (message: string, autoClose: number = 3000) => {
-  toast.info(message, { autoClose: autoClose })
+  toast.info(message, { autoClose })
 }
 export const throttledToastError = useThrottle(() => {
   handleToastError(`Please check the network and try again or later!`)
