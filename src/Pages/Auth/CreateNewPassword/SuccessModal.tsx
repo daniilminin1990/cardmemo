3a62b7a3737f20b1dc82aaade11f5bc7f27cee90
Daniil <daniilminin1990@yandex.ru>
2024-05-30 08:27:29 +0500

    refactor SuccessModal.tsx, add styles

--- src/Pages/Auth/CreateNewPassword/SuccessModal.tsx
+++ src/Pages/Auth/CreateNewPassword/SuccessModal.tsx
@@ -2,18 +2,32 @@ import Typography from '@/components/ui/Typography/Typography'
 import { Button } from '@/components/ui/button'
 import { Modal } from '@/components/ui/modal/modal'
 import { path } from '@/router/path'
+import { router } from '@/router/router'
 
 type Props = {
   open: boolean
+  setOpen: (value: boolean) => void
 }
 
-export const SuccessModal = ({ open }: Props) => {
+export const SuccessModal = ({ open, setOpen }: Props) => {
+  const handleOnClose = () => {
+    router.navigate(path.login)
+    setOpen(false)
+  }
+
   return (
     <>
-      <Modal onOpenChange={() => {}} open={open} title={'Password changed successfully'}>
-        <Button as={'a'} fullWidth href={path.login} style={{ textDecoration: 'none' }}>
-          <Typography variant={'body2'}>Return to login</Typography>
-        </Button>
+      <Modal
+        onOpenChange={handleOnClose}
+        open={open}
+        style={{ width: '420px' }}
+        title={'Password changed successfully'}
+      >
+        <div style={{ padding: '20px 24px' }}>
+          <Button as={'a'} fullWidth href={path.login} style={{ textDecoration: 'none' }}>
+            <Typography variant={'body2'}>Return to login</Typography>
+          </Button>
+        </div>
       </Modal>
     </>
   )
