6d6cdf0d3ac578a2d303f44a977bd6f28347dde7
Сергей <52825673+SiarheiHaurylchyk@users.noreply.github.com>
2024-05-30 21:04:06 +0300

    change Theme (#238)

---
+++ src/components/ui/changeTheme/Context.tsx
@@ -0,0 +1,19 @@
+import { Dispatch, SetStateAction, createContext, useState } from 'react'
+
+type UserContextProps = {
+  setTheme: Dispatch<SetStateAction<string>>
+  theme: string
+}
+export const UserContext = createContext<UserContextProps | null>(null)
+
+const Context = (props: any) => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'moon')
+  const value = {
+    setTheme,
+    theme,
+  }
+
+  return <UserContext.Provider value={value}>{props.children}</UserContext.Provider>
+}
+
+export default Context
