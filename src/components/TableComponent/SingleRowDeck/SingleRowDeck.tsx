aaf0b7eed731177957855fda7b686ea1ad0f0df5
BladeDancer <69kir969@mail.ru>
2024-05-31 22:38:31 +0300

    rebuild deleteModal

--- src/components/TableComponent/SingleRowDeck/SingleRowDeck.tsx
+++ src/components/TableComponent/SingleRowDeck/SingleRowDeck.tsx
@@ -1,17 +1,19 @@
 import { Fragment, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 import Edit2Outline from '@/assets/icons/svg/Edit2Outline'
 import PlayCircleOutline from '@/assets/icons/svg/PlayCircleOutline'
 import TrashOutline from '@/assets/icons/svg/TrashOutline'
+import { DeleteModal } from '@/components/ModalsForTable/DeleteModal/DeleteModal'
 import { ModalAddEditDeck } from '@/components/ModalsForTable/ModalAddEditDeck'
-import { ModalDeleteDeck } from '@/components/ModalsForTable/ModalDeleteDeck'
 import Typography from '@/components/ui/Typography/Typography'
 import { Button } from '@/components/ui/button'
 import { Table } from '@/components/ui/table'
 import { path } from '@/router/path'
+import { router } from '@/router/router'
 import { useMeQuery } from '@/services/auth/auth.service'
 import { Deck } from '@/services/decks/deck.types'
+import { useDeleteDeckMutation } from '@/services/decks/decks.service'
 import clsx from 'clsx'
 
 import s from '@/components/TableComponent/tableSingleRow.module.scss'
@@ -23,12 +25,32 @@ export const SingleRowDeck = ({ item }: Props) => {
   const { data: meData } = useMeQuery()
   const [isUpdateModal, setIsUpdateModal] = useState(false)
   const [isDeleteModal, setIsDeleteModal] = useState(false)
+
   const updatedAr = new Date(item.updated).toLocaleDateString('ru-RU')
+  const [deleteDeck] = useDeleteDeckMutation()
+  const { deckId } = useParams()
+  const onDeleteDeckHandler = () => {
+    deleteDeck({ id: item.id })
+    setIsDeleteModal(true)
+    if (deckId) {
+      router.navigate(path.decks)
+    }
+  }
 
   return (
     <Fragment key={item.id}>
       <ModalAddEditDeck item={item} open={isUpdateModal} setOpen={setIsUpdateModal} />
-      <ModalDeleteDeck item={item} open={isDeleteModal} setIsDeleteModal={setIsDeleteModal} />
+      <DeleteModal
+        deleteFn={onDeleteDeckHandler}
+        open={isDeleteModal}
+        setOpen={setIsDeleteModal}
+        title={'Delete Deck'}
+      >
+        <Typography variant={'h1'}>{item.name}</Typography>
+        <Typography variant={'body1'}>
+          Do you really want to delete deck? All cards will be deleted.
+        </Typography>
+      </DeleteModal>
       <Table.Row key={item.id}>
         <Table.Cell className={clsx(item?.cardsCount === 0 && s.disabledCell)}>
           <Typography as={Link} className={s.imgWrapper} to={`${path.decks}/${item.id}`}>
