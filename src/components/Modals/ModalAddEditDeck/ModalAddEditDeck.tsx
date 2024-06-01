057af10fb59e7a6a76d0985fba9ce41f128c30aa
BladeDancer <69kir969@mail.ru>
2024-06-01 13:50:59 +0300

    fix padding and locales

--- src/components/Modals/ModalAddEditDeck/ModalAddEditDeck.tsx
+++ src/components/Modals/ModalAddEditDeck/ModalAddEditDeck.tsx
@@ -19,7 +19,6 @@ import { Modal } from '@/components/ui/modal/modal'
 import { useQueryParams } from '@/hooks/useQueryParams'
 import { Deck } from '@/services/decks/deck.types'
 import { useCreateDeckMutation, useUpdateDeckMutation } from '@/services/decks/decks.service'
-import { DevTool } from '@hookform/devtools'
 
 import s from './AddEditDeckModal.module.scss'
 
@@ -82,7 +81,6 @@ export const ModalAddEditDeck = memo((props: ModalAddEditProps) => {
   return (
     <>
       {loadingStatus && <LoadingBar />}
-      <DevTool control={control} />
       <Modal
         className={s.customClass}
         onOpenChange={handleOnClose}
