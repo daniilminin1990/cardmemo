348052d41b954793c0f8767f65e4609969fc831e
andreyshalomitsky <andreyshalomitsky@gmail.com>
2024-07-01 12:12:36 +0500

    create DatePicker ver 5

--- src/components/ui/DatePicker/lib/helpers/getInputValueDate.ts
+++ src/components/ui/DatePicker/lib/helpers/getInputValueDate.ts
@@ -2,7 +2,5 @@ import { RangeDate } from '@/components/ui/DatePicker/DatePicker'
 import { getInputValueFromDate } from '@/components/ui/DatePicker/lib/utils'
 
 export const getInputValueDate = (value: RangeDate): string => {
-  return value.startDate === value.endDate
-    ? `${getInputValueFromDate(value.startDate)}`
-    : `${getInputValueFromDate(value.startDate)} - ${getInputValueFromDate(value.endDate)}`
+  return `${getInputValueFromDate(value.startDate)} - ${getInputValueFromDate(value.endDate)}`
 }
